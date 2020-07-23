0f5f69ddd7a898fd6f76ddff0f8808fedadda7b4
Ravi3253 <35370662+Ravi3253@users.noreply.github.com>
2020-07-23 12:23:05 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    $total = 0;
+    if(isset($_POST['submit']))
+    {
+        $val1 = $_POST['value1'];
+        $val2 = $_POST['value2'];
+        $total = $val1+$val2;
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Seao Interalia - Home Page</title>
+    </head>
+    <body>
+        <h3>Sum of 2 numbers</h3>
+        <form method="post">
+            <label>    Value 1</label>
+            <input type="text" name="value1" value=""><br/>
+            <h3>Sum of 2 numbers</h3>
+            <label>    Value 1</label>
+            <input type="text" name="value1" value=""><br/>
+            <label>    Total is: <?php echo $total; ?></label><br/>
+            <input  type="submit" name="submit" value="Submit"/>
+           
+        </form>
+    </body>
+</html>
